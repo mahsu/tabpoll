46ec05aaa0b193e71ae8032237101af046a5af6d
Matthew Hsu <gears88@gmail.com>
2015-09-15 10:54:16 -0400

    reference error from missing let

--- js/main.js
+++ js/main.js
@@ -264,7 +264,7 @@ function hashColor(str) {
     if (str) {
         var hash = 0;
         if (str.length == 0) return hash;
-        for (i = 0; i < str.length; i++) {
+        for (let i = 0; i < str.length; i++) {
             var char = str.charCodeAt(i);
             hash = ((hash << 5) - hash) + char;
             hash = hash & hash;
